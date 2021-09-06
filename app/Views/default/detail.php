5ae630cbca91fe03f1baafd6ddf0b8c7d11445ad
Awaludin-Pamungkas <awalks141@gmail.com>
2021-09-06 23:03:34 +0700

    update-test

--- app/Views/default/detail.php
+++ app/Views/default/detail.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/appstyle.css">
-    <title>EasyCVMaker tes ke dua</title>
+    <title>EasyCVMaker coba rubah isi file</title>
 </head>
 
 <body>
