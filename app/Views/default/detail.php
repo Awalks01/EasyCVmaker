c99a4b3700c933fc353074c022c3bd0cd329ce7a
Awaludin-Pamungkas <awalks141@gmail.com>
2021-09-06 22:58:23 +0700

    test update

--- app/Views/default/detail.php
+++ app/Views/default/detail.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/appstyle.css">
-    <title>EasyCVMaker</title>
+    <title>EasyCVMaker tes ke dua</title>
 </head>
 
 <body>
@@ -284,6 +284,6 @@
     </script>
 
 </body>
-<!-- just test -->
+
 
 </html>
